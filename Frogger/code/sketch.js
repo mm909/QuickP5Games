ac08a5a0c36de6d64f6fb9f1cf064201d481abbb
mm909 <Bobar312@gmail.com>
2018-11-05 23:41:28 -0800

    Started making frogger

--- Frogger/code/sketch.js
+++ Frogger/code/sketch.js
@@ -10,7 +10,7 @@ function setup() {
   createCanvas(500, 500);
   frog = new Frog();
   for (var i = 0; i < carCount; i++) {
-    car[i] = new Car();
+  	car[i] = new Car();
   }
 }
 
@@ -19,78 +19,78 @@ function draw() {
   frog.collide();
   frog.show();
   for (var i = car.length - 1; i >= 0; i--) {
-    car[i].show();
-    car[i].update();
-    car[i].bounds();
+	  car[i].show();
+	  car[i].update();
+	  car[i].bounds();
   }
 }
 
 function Car() {
-  this.row = floor(random(3));
-  this.x = 0;
-  this.y = 0;
-  this.width = random(20, 50);
-  this.height = 20;
-  this.dir = 0;
-  this.speed = 5;
-  this.x = random(0, width);
-  this.y = height - floor((height / 2) / 20) * 20 - (80 * this.row);
-  this.c = color(100, 100, 255);
+	this.row = floor(random(3));
+	this.x = 0;
+	this.y = 0;
+	this.width = random(20,50);
+	this.height = 20;
+	this.dir = 0;
+	this.speed = 5;
+	this.x = random(0,width);
+	this.y = height - floor((height/2) / 20) * 20 -(80 * this.row);
+	this.c = color(100,100,255);
 
-  if (this.row == 0 || this.row == 2) this.dir = 1;
-  else this.dir = -1;
+	if(this.row == 0 || this.row == 2) this.dir = 1;
+	else this.dir = -1;
 
-  this.show = function() {
-    fill(this.c);
-    rect(this.x, this.y, this.width, this.height);
-  }
+	this.show = function() {
+		fill(this.c);
+		rect(this.x,this.y,this.width,this.height);
+	}
 
-  this.update = function() {
-    this.x = this.x + this.speed * this.dir;
-  }
+	this.update = function() {
+		this.x = this.x + this.speed * this.dir;
+	}
 
-  this.bounds = function() {
-    if (this.x > width) this.x = 0 - this.width;
-    if (this.x < 0 - this.width) this.x = width;
-  }
+	this.bounds = function() {
+		if(this.x > width) this.x = 0;
+		if(this.x < 0) this.x = width;
+	}
 }
 
 function Frog() {
-  this.size = 20;
-  this.x = width / 2;
-  this.y = height - this.size;
-  this.c = color(100, 255, 100)
+	this.size = 20;
+	this.x = width/2;
+	this.y = height - this.size;
+	this.c = color(100,255,100)
 
-  this.show = function() {
-    noStroke();
-    fill(this.c);
-    rect(this.x, this.y, this.size, this.size);
-  }
+	this.show = function() {
+		noStroke();
+		fill(this.c);
+		rect(this.x,this.y,this.size,this.size);
+	}
 
-  this.collide = function() {
-    for (var i = 0; i < carCount; i++) {
-      var hit = collideRectRect(this.x - 1, this.y - 1, this.size - 1, this.size - 1, car[i].x - 1, car[i].y - 1, car[i].width - 1, car[i].height - 1)
-      if (hit) {
-        this.c = color(255, 100, 100);
-        this.y = this.y + this.size;
-      } else {
-        this.c = color(100, 255, 100);
-      }
-    }
-  }
+	this.collide = function() {
+		for(var i = 0; i < carCount; i++){
+			var hit = collideRectRect(this.x-1,this.y-1,this.size-1,this.size-1,car[i].x-1,car[i].y-1,car[i].width-1,car[i].height-1)
+			if(hit) {
+				this.c = color(255,100,100);
+				this.y = this.y + this.size;
+			} else {
+				this.c = color(100,255,100);
+			}
+		}
+	}
 }
 
-function keyPressed() {
-  if (keyIsDown(LEFT_ARROW)) {
-    frog.x = frog.x - frog.size;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    frog.x = frog.x + frog.size;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    frog.y = frog.y - frog.size;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    frog.y = frog.y + frog.size;
-  }
+function keyPressed(){
+	if(keyIsDown(LEFT_ARROW)){
+		frog.x = frog.x - frog.size;
+	}
+	if(keyIsDown(RIGHT_ARROW)){
+		frog.x = frog.x + frog.size;
+	}
+	if(keyIsDown(UP_ARROW)){
+		frog.y = frog.y - frog.size;
+	}
+	if(keyIsDown(DOWN_ARROW)){
+		frog.y = frog.y + frog.size;
+	}
 }
